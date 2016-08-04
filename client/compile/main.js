c87a22182f47b30214900e6571025915c7f4fd54
fiveSecondsRealMan <fiveSecondsRealMan>
2016-08-04 16:58:45 +0800

    initial commit

---
+++ client/compile/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _a = require('./a.js');
+
+var _a2 = _interopRequireDefault(_a);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _a2.default)();
---
+++ client/src/main.js
@@ -0,0 +1,4 @@
+import a from './a.js';
+var a = 10;
+var d = 20;
+d();
